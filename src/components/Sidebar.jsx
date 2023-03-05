02da5653b22919ebcb3926b42915cd9fd264d1a1
Amirhossein Zarei <103286828+AmirZarei2002@users.noreply.github.com>
2023-03-05 09:36:56 +0330

    Sidebar created

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,53 @@
+import { Stack } from '@mui/material';
+
+import { categories } from '../utils/constants';
+
+const selectedCategory = 'New';
+
+export default function Sidebar() {
+    return (
+        <Stack
+            direction="row"
+            sx={{
+                overflow: 'auto',
+                height: { sx: 'auto', md: '95%' },
+                flexDirection: { md: 'column' },
+            }}
+        >
+            {categories.map((category) => (
+                <button
+                    type="button"
+                    style={{
+                        background:
+                            category.name === selectedCategory && '#FC1503',
+                        color: 'white',
+                    }}
+                    key={category.name}
+                    className="category-btn"
+                >
+                    <span
+                        style={{
+                            color:
+                                category.name === selectedCategory
+                                    ? 'white'
+                                    : 'red',
+                            marginRight: '15px',
+                        }}
+                    >
+                        {category.icon}
+                    </span>
+                    <span
+                        style={{
+                            opacity:
+                                category.name === selectedCategory
+                                    ? '1'
+                                    : '0.8',
+                        }}
+                    >
+                        {category.name}
+                    </span>
+                </button>
+            ))}
+        </Stack>
+    );
+}
